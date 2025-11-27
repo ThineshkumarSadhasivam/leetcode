302e25251721783d186fb351af6b0c9153dabc0b
ThineshkumarSadhasivam <thineshkumarsadhasivam@gmail.com>
2025-11-27 18:36:00 +0530

    2525-COUNT-NUMBER-OF-DISTINCT-INTEGERS-AFTER-REVERSE-OPERATIONS Stats: Time: 64 ms (97.6%), Space: 68.2 MB (98.05%) - LeetHub

--- 2525-count-number-of-distinct-integers-after-reverse-operations/2525-count-number-of-distinct-integers-after-reverse-operations.cpp
+++ 2525-count-number-of-distinct-integers-after-reverse-operations/2525-count-number-of-distinct-integers-after-reverse-operations.cpp
@@ -8,17 +8,16 @@ public:
             res.push_back(nums[i]);
         }
         for(int i=0;i<n;i++){
-            int temp=nums[i];
             int rev=0;
-            while (temp > 0) {
-                rev = rev * 10 + (temp % 10); 
-                temp /= 10; 
+            while (nums[i] > 0) {
+                rev = rev * 10 + (nums[i] % 10); 
+                nums[i] /= 10; 
             }
             res.push_back(rev);
         }
         sort(res.begin(),res.end());
-        for(int i=1;i<res.size();i++){
-            if(res[i]!=res[i-1])
+        for(int i=0;i<res.size()-1;i++){
+            if(res[i]!=res[i+1])
                 count++;
         }
         return count+1;
