2ff37c139a16050e94874522f6e63f51404e6159
ThineshkumarSadhasivam <thineshkumarsadhasivam@gmail.com>
2025-11-28 23:08:57 +0530

    2836-NEITHER-MINIMUM-NOR-MAXIMUM Stats: Time: 0 ms (100%), Space: 97.1 MB (56.53%) - LeetHub

--- 2836-neither-minimum-nor-maximum/2836-neither-minimum-nor-maximum.cpp
+++ 2836-neither-minimum-nor-maximum/2836-neither-minimum-nor-maximum.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
     int findNonMinOrMax(vector<int>& nums) {
+        //int n=nums.size();
+        //int m=-1;
+        //vector<int>res;
+        //int min = *min_element(nums.begin(), nums.end());
+        //int max=  *max_element(nums.begin(), nums.end());
+        //for(int i=0;i<n;i++){
+            //if(nums[i]!=min && nums[i]!=max)
+                //res.push_back(nums[i]);
+        //}
+        //int k=res.size();
+        //for(int i=0;i<k;i++){
+            //m=res[i];
+            //break;
+        //}
+        //return m;
         int n=nums.size();
-        int m=0;
-        vector<int>res;
-        int min = *min_element(nums.begin(), nums.end());
-        int max=  *max_element(nums.begin(), nums.end());
-        for(int i=0;i<n;i++){
-            if(nums[i]!=min && nums[i]!=max)
-                res.push_back(nums[i]);
+        if(n<3)
+            return -1;
+        int min=*min_element(nums.begin(),nums.end());
+        int max=*max_element(nums.begin(),nums.end());
+        for(int i:nums){
+            if(i!=min && i!=max)
+                return i;
         }
-        int k=res.size();
-        if(k==0)
-            m=-1;
-        for(int i=0;i<k;i++){
-            m=res[i];
-            break;
-        }
-        return m;
+        return -1;
     }
 };
