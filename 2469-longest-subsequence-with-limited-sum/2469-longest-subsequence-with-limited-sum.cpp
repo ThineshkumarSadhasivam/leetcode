b6a69c61cd027ad6bad5b6527efb222716561a8c
Thineshkumar S <thineshkumarsadhasivam@gmail.com>
2026-01-16 19:02:20 +0530

    2469-LONGEST-SUBSEQUENCE-WITH-LIMITED-SUM Stats: Time: 45 ms (5.06%), Space: 17.9 MB (65.45%) - LeetHub

--- 2469-longest-subsequence-with-limited-sum/2469-longest-subsequence-with-limited-sum.cpp
+++ 2469-longest-subsequence-with-limited-sum/2469-longest-subsequence-with-limited-sum.cpp
@@ -3,10 +3,11 @@ public:
     vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
         vector<int>res;
         sort(nums.begin(),nums.end());
-        for(int i=0;i<queries.size();i++){
-            int count=0;
-            int sum=0;
-            for(int j=0;j<nums.size();j++){
+        int n=queries.size();
+        int m=nums.size();
+        for(int i=0;i<n;i++){
+            int count=0,sum=0;
+            for(int j=0;j<m;j++){
                 sum+=nums[j];
                 if(sum<=queries[i])                   
                     count++;
