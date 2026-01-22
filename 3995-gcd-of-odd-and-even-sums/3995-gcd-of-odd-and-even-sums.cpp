f312859ad75a7c7190a9b8ff676f54e9c8bfe4e5
Thineshkumar S <thineshkumarsadhasivam@gmail.com>
2026-01-22 12:01:57 +0530

    3995-GCD-OF-ODD-AND-EVEN-SUMS Stats: Time: 0 ms (100%), Space: 8.6 MB (8.19%) - LeetHub

--- 3995-gcd-of-odd-and-even-sums/3995-gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/3995-gcd-of-odd-and-even-sums.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        int odd=1,even=2;
-        vector<int>res;
-        int oddtot=0,eventot=0;
-        for(int i=0;i<n;i++){
-            oddtot+=odd;
-            odd+=2;
-            eventot+=even;
-            even+=2;
-        }
-        int minimum=min(oddtot,eventot);
-        for(int i=1;i<=minimum;i++){
-            if((oddtot%i==0) && (eventot%i==0))
-                res.push_back(i);
-        }
-        int gcd=*max_element(res.begin(),res.end());
-        return gcd;
+        //int odd=1,even=2;
+        //vector<int>res;
+        //int oddtot=0,eventot=0;
+        //for(int i=0;i<n;i++){
+            //oddtot+=odd;
+            //odd+=2;
+            //eventot+=even;
+            //even+=2;
+        //}
+        //int minimum=min(oddtot,eventot);
+        //for(int i=1;i<=minimum;i++){
+            //if((oddtot%i==0) && (eventot%i==0))
+                //res.push_back(i);
+        //}
+        //int gcd=*max_element(res.begin(),res.end());
+        //return gcd;
+        return n;
     }
 };
