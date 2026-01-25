156c96284253a7f0897dba07792dece1bc0ddec7
Thineshkumar S <thineshkumarsadhasivam@gmail.com>
2026-01-25 11:24:54 +0530

    4012-EARLIEST-TIME-TO-FINISH-ONE-TASK Stats: Time: 0 ms (100%), Space: 37.4 MB (25.8%) - LeetHub

--- 4012-earliest-time-to-finish-one-task/4012-earliest-time-to-finish-one-task.cpp
+++ 4012-earliest-time-to-finish-one-task/4012-earliest-time-to-finish-one-task.cpp
@@ -3,14 +3,17 @@ public:
     int earliestTime(vector<vector<int>>& tasks) {
         int n=tasks.size();
         int m=tasks[0].size();
-        int res=INT_MAX;
+        vector<int>res;
+        //int res=INT_MAX;
         for(int i=0;i<n;i++){
             int sum=0;
             for(int j=0;j<m;j++){
                 sum+=tasks[i][j];
             }
-            res=min(sum,res);
+            //res=min(sum,res);
+            res.push_back(sum);
         }
-        return res;
+        sort(res.begin(),res.end());
+        return res[0];
     }
 };
