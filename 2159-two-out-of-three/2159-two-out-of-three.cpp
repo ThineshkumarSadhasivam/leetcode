e36530a37b952c03dfe81864cc878c1da7777b5e
Thineshkumar S <thineshkumarsadhasivam@gmail.com>
2026-02-23 14:06:16 +0530

    2159-TWO-OUT-OF-THREE Stats: Time: 4 ms (66.07%), Space: 29.8 MB (85.27%) - LeetHub

--- 2159-two-out-of-three/2159-two-out-of-three.cpp
+++ 2159-two-out-of-three/2159-two-out-of-three.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     vector<int> twoOutOfThree(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3) {
         vector<int>res;
-        vector<int>res1;
-        vector<int>res2;
         for(int i=0;i<nums1.size();i++){
             for(int j=0;j<nums2.size();j++){
                 if(nums1[i]==nums2[j])
