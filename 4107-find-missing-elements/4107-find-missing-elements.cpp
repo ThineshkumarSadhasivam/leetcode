a8be5367b90e078bad35c21fbe1cd5355c6ccc47
Thineshkumar S <thineshkumarsadhasivam@gmail.com>
2026-03-06 21:49:22 +0530

    4107-FIND-MISSING-ELEMENTS Stats: Time: 3 ms (52.18%), Space: 32.4 MB (47.5%) - LeetHub

--- 4107-find-missing-elements/4107-find-missing-elements.cpp
+++ 4107-find-missing-elements/4107-find-missing-elements.cpp
@@ -1,22 +1,40 @@
 class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
-        vector<int>res;
-        int n=nums.size();
-        int mn = *min_element(nums.begin(), nums.end());
+        // vector<int>res;
+        // int n=nums.size();
+        // int mn = *min_element(nums.begin(), nums.end());
+        // int mx = *max_element(nums.begin(), nums.end());
+        // for(int i=mn;i<=mx;i++){
+        //     bool ans=false;
+        //     //for(int num:nums){
+        //     for(int j=0;j<n;j++){
+        //         //if(num==i){
+        //         if(nums[j]==i){
+        //             ans=true;
+        //             break;
+        //         }
+        //     }
+        //         if(!ans){
+        //             res.push_back(i);
+        //         }
+        // }
+        // return res;
+        
+        int n = nums.size();
+        vector<int> res;
         int mx = *max_element(nums.begin(), nums.end());
-        for(int i=mn;i<=mx;i++){
-            bool ans=false;
-            for(int j=0;j<n;j++){
-                if(nums[j]==i){
-                    ans=true;
-                    break;
-                }
-            }
-                if(!ans){
-                    res.push_back(i);
-                }
+        int mn = *min_element(nums.begin(), nums.end());
+        vector<int> freq(mx + 1, 0);
+        for(int x : nums){
+            freq[x]++;
         }
+
+        for(int i = mn; i <= mx; i++){
+            if(freq[i] == 0)
+                res.push_back(i);
+        }
+
         return res;
     }
 };
