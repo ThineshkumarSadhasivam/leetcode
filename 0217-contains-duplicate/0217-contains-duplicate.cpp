60b33e0f20bc765991d76c60e77e3d49ce387c6e
Thineshkumar S <thineshkumarsadhasivam@gmail.com>
2026-03-26 15:01:49 +0530

    0217-CONTAINS-DUPLICATE Stats: Time: 47 ms (65.16%), Space: 90.9 MB (28.2%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
-        for(int i=1;i<n;i++){
-            if(nums[i]==nums[i-1])
-                return true;
-        }
-        return false;
+    //     int n=nums.size();
+    //     sort(nums.begin(),nums.end());
+    //     for(int i=1;i<n;i++){
+    //         if(nums[i]==nums[i-1])
+    //             return true;
+    //     }
+    //     return false;
+    // }
+    std::unordered_set<int>set1;
+    for(int x:nums){
+        if(set1.find(x)==set1.end())
+            set1.insert(x);
+        else
+            return true;
     }
+    return false;
+    }
+
 };
