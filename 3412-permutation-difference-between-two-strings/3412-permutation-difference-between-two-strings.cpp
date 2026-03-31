7bc98ba5eb3d039c1b49b99e1da9ee1b26f0394a
Thineshkumar S <thineshkumarsadhasivam@gmail.com>
2026-03-31 09:59:26 +0530

    3412-PERMUTATION-DIFFERENCE-BETWEEN-TWO-STRINGS Stats: Time: 2 ms (37.29%), Space: 10 MB (20.3%) - LeetHub

--- 3412-permutation-difference-between-two-strings/3412-permutation-difference-between-two-strings.cpp
+++ 3412-permutation-difference-between-two-strings/3412-permutation-difference-between-two-strings.cpp
@@ -1,13 +1,26 @@
 class Solution {
 public:
     int findPermutationDifference(string s, string t) {
+        // int sum=0;
+        // for(int i=0;i<s.size();i++){
+        //     for(int j=0;j<t.size();j++){
+        //         if(s[i]==t[j])
+        //             sum+=abs(i-j);
+        //     }
+        // } 
+        // return sum;
+
         int sum=0;
+        std::unordered_map<char,int>mp;
+
+        for(int i=0;i<t.size();i++){
+            mp[t[i]]=i;
+        }
+
         for(int i=0;i<s.size();i++){
-            for(int j=0;j<t.size();j++){
-                if(s[i]==t[j])
-                    sum+=abs(i-j);
-            }
-        } 
+            sum+=abs(i-mp[s[i]]);
+        }
+
         return sum;
     }
 };
