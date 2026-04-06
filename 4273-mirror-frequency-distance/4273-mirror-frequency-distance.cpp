bed4908083d14b6c59182481f7baaf9b3448d930
Thineshkumar S <thineshkumarsadhasivam@gmail.com>
2026-04-06 09:56:42 +0530

    4273-MIRROR-FREQUENCY-DISTANCE Stats: Time: 16 ms (94.74%), Space: 58.4 MB (15.79%) - LeetHub

--- 4273-mirror-frequency-distance/4273-mirror-frequency-distance.cpp
+++ 4273-mirror-frequency-distance/4273-mirror-frequency-distance.cpp
@@ -1,47 +1,42 @@
-#include <vector>
-#include <string>
-#include <cmath>
-using namespace std;
-
 class Solution {
 public:
     int mirrorFrequency(string s) {
-        // vector<int> freq(128, 0);
+        vector<int> freq(128, 0);
 
-        // for (char c : s) {
-        //     freq[c]++;
-        // }
-
-        // int count = 0;
-
-        // for (char c = 'a'; c <= 'm'; c++) {
-        //     char m = 'a' + ('z' - c);
-        //     count += abs(freq[c] - freq[m]);
-        // }
-
-        // for (char c = '0'; c <= '4'; c++) {
-        //     char m = '0' + ('9' - c);
-        //     count += abs(freq[c] - freq[m]);
-        // }
-
-        // return count;
-
-        std::unordered_map<char,int>freq;
-        int count=0;
-        
-        for(char c:s)
+        for (char c : s) {
             freq[c]++;
-        
-        for(char c='a';c<='m';c++){
-            char m='a'+('z'-c);
-            count+=abs(freq[c]-freq[m]);
         }
 
-        for(char c='0';c<='4';c++){
-            char m='0'+('9'-c);
-            count+=abs(freq[c]-freq[m]);
+        int count = 0;
+
+        for (char c = 'a'; c <= 'm'; c++) {
+            char m = 'a' + ('z' - c);
+            count += abs(freq[c] - freq[m]);
         }
+
+        for (char c = '0'; c <= '4'; c++) {
+            char m = '0' + ('9' - c);
+            count += abs(freq[c] - freq[m]);
+        }
+
         return count;
+
+        // std::unordered_map<char,int>freq;
+        // int count=0;
+
+        // for(char c:s)
+        //     freq[c]++;
+        
+        // for(char c='a';c<='m';c++){
+        //     char m='a'+('z'-c);
+        //     count+=abs(freq[c]-freq[m]);
+        // }
+
+        // for(char c='0';c<='4';c++){
+        //     char m='0'+('9'-c);
+        //     count+=abs(freq[c]-freq[m]);
+        // }
+        // return count;
 
     }
 };
