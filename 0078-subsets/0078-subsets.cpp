390cab8a768941d7b25a6d00f35dc0d7006aec31
Thineshkumar S <thineshkumarsadhasivam@gmail.com>
2026-06-27 21:40:11 +0530

    0078-SUBSETS Stats: Time: 0 ms (100%), Space: 9.8 MB (96.11%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -17,3 +17,20 @@ public:
         return res;
     }
 };
+//     void f(int index,vector<int>& nums,vector<vector<int>>& res,vector<int>& cur){
+//         if(index==nums.size()){
+//             res.push_back(cur);
+//             return;
+//         }
+//         f(index+1,nums,res,cur);
+//         cur.push_back(nums[index]);
+//         f(index+1,nums,res,cur);
+//         cur.pop_back();
+//     }
+//     vector<vector<int>> subsets(vector<int>& nums) {
+//         vector<vector<int>>res;
+//         vector<int>cur;
+//         f(0,nums,res,cur);
+//         return res;
+//     }
+// };
