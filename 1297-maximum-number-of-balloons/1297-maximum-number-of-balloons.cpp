78e7f0d822f9ba599221fa553eaae1c5a0c712ad
Thineshkumar S <thineshkumarsadhasivam@gmail.com>
2026-07-29 22:32:28 +0530

    1297-MAXIMUM-NUMBER-OF-BALLOONS Stats: Time: 0 ms (100%), Space: 8.9 MB (69.21%) - LeetHub

--- 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.cpp
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     int maxNumberOfBalloons(string text) {
-        int b=0,a=0,l=0,o=0,n=0;
-        for(char &c:text){
-            if(c=='b') b++;
-            else if(c=='a') a++;
-            else if(c=='l') l++;
-            else if(c=='o') o++;
-            else if(c=='n') n++;
+        // int b=0,a=0,l=0,o=0,n=0;
+        // for(char &c:text){
+        //     if(c=='b') b++;
+        //     else if(c=='a') a++;
+        //     else if(c=='l') l++;
+        //     else if(c=='o') o++;
+        //     else if(c=='n') n++;
+        // }
+        // return min({b,a,l/2,o/2,n});
+
+        int freq[26]={0};
+        for(char c:text){
+            freq[c-'a']++;
         }
-        return min({b,a,l/2,o/2,n});
+        return min(
+            {freq['b'-'a'],freq['a'-'a'],freq['l'-'a']/2,freq['o'-'a']/2,freq['n'-'a']    
+        });
     }
 };
