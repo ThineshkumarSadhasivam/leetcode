e6cdede260158f5ed454cabeba80d843cb3c6e1b
Thineshkumar S <thineshkumarsadhasivam@gmail.com>
2026-08-20 22:55:26 +0530

    2308-DIVIDE-ARRAY-INTO-EQUAL-PAIRS Stats: Time: 0 ms (100%), Space: 17.9 MB (61.34%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.cpp
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
     bool divideArray(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
-        int k=n/2,count=0;
-        for(int i=1;i<n;i++){
-            if(nums[i]==nums[i-1]){
-                count++;
-                i++;
-            }
+        //     sort(nums.begin(),nums.end());
+        //     int n=nums.size();
+        //     int k=n/2,count=0;
+        //     for(int i=1;i<n;i++){
+        //         if(nums[i]==nums[i-1]){
+        //             count++;
+        //             i++;
+        //         }
+        //     }
+        //     if(count==k)
+        //         return true;
+        //     return false;
+        // }
+        std::unordered_map<int, int> freq;
+        for (int x : nums) {
+            freq[x]++;
         }
-        if(count==k)
-            return true;
-        return false;
+        for (auto& x : freq) {
+            if (x.second % 2 != 0)
+                return false;
+        }
+        return true;
     }
 };
